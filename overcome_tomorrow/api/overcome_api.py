cecf7451229bf445210366484497197ed5ed407e
Nicolas BLANC <nicko64@hotmail.fr>
2024-03-20 16:33:40 +0100

    Design basic get models, get existing and predicted activities endpoints. Use static data for now.

--- overcome_tomorrow/api/overcome_api.py
+++ overcome_tomorrow/api/overcome_api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 
 tomorrow_app = FastAPI()
 tomorrow_app.add_middleware(
@@ -9,6 +10,84 @@ tomorrow_app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
+tags_metadata = [
+    {
+        "name": "models",
+        "description": "Operations regarding overcome-tomorrow models.",
+    },
+    {
+        "name": "activities",
+        "description": "Operations regarding activities.",
+    }
+]
+
+DEFAULT_ACTIVITY = {
+    "timestamp": ["08/10/2023 17:16"],
+    "total_anaerobic_training_effect": [0.1],
+    "enhanced_avg_speed": [4.351],
+    "avg_stroke_distance": [None],
+    "total_training_effect": [5.0],
+    "num_active_lengths": [None],
+    "max_running_cadence": [95.0],
+    "max_heart_rate": [174.0],
+    "sub_sport": ["generic"],
+    "total_descent": [78.0],
+    "start_time": ["08/10/2023 14:31"],
+    "total_distance": [42685.7],
+    "pool_length": [None],
+    178: [2724.0],
+    "total_calories": [2512.0],
+    "max_cadence": [95.0],
+    "sport": ["running"],
+    188: [3.0],
+    "avg_step_length": [1476.1],
+    "enhanced_max_speed": [7.81],
+    205: [100.0],
+    "num_lengths": [None],
+    206: [40.0],
+    207: [40.0],
+    "pool_length_unit": [None],
+    "normalized_power": [385.0],
+    "training_load_peak": [round(309.48114013671875, 2)],
+    "total_ascent": [85.0],
+    "avg_power": [379.0],
+    "total_strokes": [None],
+    "training_stress_score": [None],
+    "avg_cadence": [88.0],
+    "avg_heart_rate": [153.0],
+    "max_power": [825.0],
+    "activity_id": ["nicko64@hotmail.fr_213811102683"]
+}
+
+
+@tomorrow_app.get("/models", tags=["models"])
+def get_model_names():
+    return ["model1", "model2", "model3"]
+
+
+@tomorrow_app.get("/activities", tags=["activities"])
+def get_activity(activity_datetime: str, summarized: bool = True):
+    # TODO handle date
+    # TODO return multiple activities.
+    if summarized:
+        return [DEFAULT_ACTIVITY]
+    activity_df = pd.read_csv(
+        "overcome_tomorrow/api/static_data/nicko64@hotmail.fr_213811102683.csv", parse_dates=True, index_col="timestamp")
+    return [activity_df.to_json()]
+
+
+@tomorrow_app.get("/activities/next", tags=["activities"])
+def predict_next_activity(models: str = "DEFAULT"):
+    # TODO get activity for each model
+    return [predict_next_activity(DEFAULT_ACTIVITY) for model in models.split(",")]
+
+
+@tomorrow_app.get("/activities/next/{model}", tags=["activities"])
+def predict_next_activity(model: str):
+    # TODO get activity for given model
+    # TODO handle 'DEFAULT' model
+    return DEFAULT_ACTIVITY
 
 
 @tomorrow_app.get("/")
