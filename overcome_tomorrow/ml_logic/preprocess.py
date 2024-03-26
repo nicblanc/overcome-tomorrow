0bfc412848a440d784a39016e222483917ac3ff3
ruben chemla <benuur@outlook.fr>
2024-03-26 14:47:50 +0100

    add better preprocessing

--- overcome_tomorrow/ml_logic/preprocess.py
+++ overcome_tomorrow/ml_logic/preprocess.py
@@ -85,17 +85,63 @@ class CyclicalFeaturesActivity(TransformerMixin, BaseEstimator):
 
 def create_preproc_garmin_data(data):
 
+    # drop cols with 80% of nan
+    cols_with_80_percent_of_nan = []
+    for col in data.columns:
+        if data[col].isna().sum() >= ((data.shape[0] * 60) / 100):
+            cols_with_80_percent_of_nan.append(col)
+    data = data.drop(columns=cols_with_80_percent_of_nan)
+
     # Delete na for columns beginTimestam
     cycle_data = data.select_dtypes(include=np.datetime64).dropna()
-
+    
     # get all cols for each type
     cycle_features = data.select_dtypes(include=np.datetime64).columns
     numerical_features = data.select_dtypes(include=np.number).columns
-
+    
+    # fonction transformer
+    def fill_max_speed(row):
+        if np.isnan(row["avgSpeed"]):
+            row["avgSpeed"] = 0
+        if np.isnan(row["maxSpeed"]):
+            row["maxSpeed"] = row["avgSpeed"]
+        return row
+    fill_max_speed_by_avg = FunctionTransformer(lambda df: df.apply(fill_max_speed, axis=1))
+    
+    # delete feature useless
+    numerical_features = list(set(numerical_features)- set(['avgBikeCadence','avgPower',
+                                       'caloriesConsumed','maxPower',
+                                       'normPower','trainingStressScore']))
     # pipeline numerical features
     pipe_numerical_knn = Pipeline([
         ('knn_imputer', KNNImputer(n_neighbors=4)),
         ('robust_scaler', RobustScaler())
+    ])
+                              
+    # pipeline simple imputer fill value with 0
+    pipe_nan_to_0 = Pipeline([
+        ('simple_imputer', SimpleImputer(strategy="constant", fill_value=0)),
+        ('robust_scaler', RobustScaler())
+    ])
+    
+    # pipeline function transformer
+    pipe_fill_max_speed = Pipeline([
+        ("function_transformer", fill_max_speed_by_avg),
+        ("robust_scaler", RobustScaler())
+    ])
+    
+    # column transformer numerical
+    imputer_to_0_features = ["avgRunCadence", "moderateIntensityMinutes", 
+                            "vigorousIntensityMinutes", "distance"]
+    knn_features = list(set(numerical_features) - set(["maxSpeed", "avgSpeed", "avgRunCadence",
+                                                      "moderateIntensityMinutes", 
+                                                       "vigorousIntensityMinutes", 
+                                                       "distance"]))
+    
+    numerical_preprocessing = ColumnTransformer(transformers=[
+        ("knn_imputer", pipe_numerical_knn, knn_features),
+        ("simple_imputer_0", pipe_nan_to_0, imputer_to_0_features),
+        ("function_transform_max_speed", pipe_fill_max_speed, ["maxSpeed", "avgSpeed"])
     ])
 
     # pipeline categorial features
@@ -107,9 +153,8 @@ def create_preproc_garmin_data(data):
 
     # full preprocessing
     full_preprocessing = ColumnTransformer(transformers=[
-        ("knn_transformer", pipe_numerical_knn, numerical_features),
-        ("pipe_categorical", pipe_categorical,
-         ["sportType", "trainingEffectLabel"]),
+        ("numerical_preprocessing", numerical_preprocessing, numerical_features),
+        ("pipe_categorical", pipe_categorical, ["sportType", "trainingEffectLabel"]),
         ("cycle transform", CyclicalFeaturesSleep(), cycle_features)
     ]).set_output(transform="pandas")
     return full_preprocessing
@@ -133,17 +178,11 @@ def create_preproc_activity(activity_df):
                                                         "total_strokes",
                                                         "training_stress_score",
                                                         "avg_cadence",
-                                                        "max_power"
+                                                        "max_power",
+                                                        "total_distance",
+                                                        "total_descent"
                                                         ])
     numerical_features = list(numerical_features)
-
-    # Stamina features
-    # 205: beginning
-    # 206: ending
-    # 207: min
-    beginning_stamina_features = ["205"]
-    numerical_features = list(
-        set(numerical_features) - set(beginning_stamina_features))
 
     # pipeline knn imputer
     pipe_knn_imputer = Pipeline([
@@ -156,12 +195,65 @@ def create_preproc_activity(activity_df):
         ('simple_imputer', SimpleImputer(strategy="constant", fill_value=100)),
         ('robust_scaler', RobustScaler())
     ])
+    
+     # fonction transformer
+    def fill_max_speed(row):
+        if np.isnan(row["enhanced_avg_speed"]):
+            row["enhanced_avg_speed"] = 0
+        if np.isnan(row["enhanced_max_speed"]):
+            row["enhanced_max_speed"] = row["enhanced_avg_speed"]
+        return row
+    fill_max_speed_by_avg = FunctionTransformer(lambda df: df.apply(fill_max_speed, axis=1))
+    
+    # pipe function transformer
+    
+    pipe_fill_max_speed = Pipeline([
+        ("function_transformer", fill_max_speed_by_avg),
+        ("robust_scaler", RobustScaler())
+    ])
+    
+    # function for 188 to primary label 
+    def get_primary_label(row):
+        primary_label = {
+            0: "None",
+            1: "Recovery",
+            2: "Base",
+            3: "Tempo",
+            4: "Threshold",
+            5: "VO2max",
+            6: "Anaerobic",
+            7: "Speed",
+        }
+        row["188"] = primary_label.get(row["188"])
+        return row
+    function_transformer_label = FunctionTransformer(lambda df: df.apply(get_primary_label, axis=1))
+    
+    # pipeline primary label to col 188
+    
+    pipe_primary_label = Pipeline([
+        ('simple_imputer', SimpleImputer(strategy="constant", fill_value=0)),
+        ("transform_188", function_transformer_label),
+        ("one_hot", OneHotEncoder(sparse_output=False,
+         handle_unknown="ignore", drop="if_binary"))
+    ])
+    
+    # Column transformer numerical
+    cols_transformer = ["enhanced_max_speed", "enhanced_avg_speed"]
+    knn_features = list(set(numerical_features) - set(cols_transformer))
+    knn_features = list(set(knn_features) - set(["205", "188"]))
 
+    numerical_proces = InvertableColumnTransformer(transformers=[
+        ("imputer_100", pipe_nan_to_100, ["205"]),
+        ("knn_imputer", pipe_knn_imputer, knn_features),
+        ("function_transformer_max_speed", pipe_fill_max_speed, ["enhanced_max_speed", "enhanced_avg_speed"]),
+        ("188_label_primary", pipe_primary_label, ["188"])
+    ]).set_output(transform="pandas")
+    
     # get data and cols categorial feature
     data_cat = activity_df.select_dtypes(exclude=np.number)
     cat_features = data_cat.columns
     cat_features = list(set(cat_features) - set(["pool_length_unit"]))
-
+    
     # pipeline categorical
 
     pipe_onehot = Pipeline([
@@ -169,13 +261,18 @@ def create_preproc_activity(activity_df):
         ("one_hot", OneHotEncoder(sparse_output=False,
          handle_unknown="ignore", drop="if_binary"))
     ])
-
+    
+    # Column transformer categorial
+    
+    categorial_proces = InvertableColumnTransformer(transformers=[
+        ("pipe_onehot", pipe_onehot, ["sport"])
+    ]).set_output(transform="pandas")
+    
+    # Full preprocessor
     full_proces = InvertableColumnTransformer(transformers=[
-        ("knn_imputer", pipe_knn_imputer, numerical_features),
-        ("cat_encoder", pipe_onehot, ["sport"]),
-        ("cycle_encoder", CyclicalFeaturesActivity(),
-         ["timestamp", "start_time"]),
-        ("imputer 100", pipe_nan_to_100, beginning_stamina_features)
+        ("numerical_proces", numerical_proces, numerical_features),
+        ("categorial_proces", pipe_onehot, ["sport"]),
+        ("cycle_encoder", CyclicalFeaturesActivity(), ["timestamp", "start_time"])
     ]).set_output(transform="pandas")
 
     return full_proces
